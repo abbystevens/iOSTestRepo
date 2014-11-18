731952d0049ae14a358dee468b01b4934ae79005
abbystevens <adeats@gmail.com>
2014-11-17 20:32:46 -0800

    first commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Abby Stevens on 11/17/14.
+//  Copyright (c) 2014 Abby Stevens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Abby Stevens on 11/17/14.
+//  Copyright (c) 2014 Abby Stevens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
